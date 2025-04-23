451af06ddf34f11892916fe3aba6d5086b12be88
ahlawatdeepak <deepakahlawat735729@gmail.com>
2025-04-23 23:10:28 +0530

    answer8

---
+++ answer8.js
@@ -0,0 +1,10 @@
+function personInfo() {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+}
+  
+const person = {
+    name: "John",
+    age: 30
+};
+  
+personInfo.call(person); // Output: Name: John, Age: 30
